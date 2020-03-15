9359bda808ad768af3d85d02070acfa2c803de52
Andrzej Misiewicz <andrzej@pandasoft.pl>
2020-03-15 18:05:01 +0100

    add template

---
+++ templates/wp-config-extra.php
@@ -0,0 +1,13 @@
+<?php
+// wp-config-extra
+define('WP_DEBUG', true);
+
+// define('WP_HOME','http://example.localhost');
+// define('WP_SITEURL','http://example.localhost');
+
+// if (isset($_SERVER["HTTP_X_FORWARDED_PROTO"]) && $_SERVER["HTTP_X_FORWARDED_PROTO"]  == "https") {
+// 	$_SERVER["HTTPS"]="on";
+// }
+// if (isset($_SERVER["HTTP_X_FORWARDED_HOST"])) {
+// 	$_SERVER["HTTP_HOST"] = $_SERVER["HTTP_X_FORWARDED_HOST"];
+// }
